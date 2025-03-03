3b53d71358113ecc4c0974edc020aef08b47bdff
riteshdhapate <rieshdhapatepatil@gmail.com>
2025-03-03 11:18:04 +0530

    added data vaiabel

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+let data="";
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/data", (req, res) => {
+  res.send(data);
+});
 
 app.post("/webhook", (req, res) => {
+    data = JSON.stringify(req.body, null, 2);
     console.log(JSON.stringify(req.body, null, 2));
     res.send("Webhook received!");
 });
